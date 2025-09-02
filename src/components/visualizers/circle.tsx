4eee7157a019245f161b494cb9a4dc8e591e4349
oncet <camilorivera86@gmail.com>
2025-09-02 16:45:29 -0300

    pattern

--- src/components/visualizers/circle.tsx
+++ src/components/visualizers/circle.tsx
@@ -3,7 +3,12 @@ type CircleProps = {
 };
 
 export default function Circle({ dataArray }: CircleProps) {
-  const [valueA, valueD] = dataArray;
+  const normalizeValue = (value: number): number => {
+    return Math.min(Math.max((value / 255) * 19 + 1, 1), 20);
+  };
+
+  const [valueA] = dataArray;
+  const normalizedValueA = normalizeValue(valueA);
 
   return (
     <svg
@@ -12,8 +17,12 @@ export default function Circle({ dataArray }: CircleProps) {
       viewBox="0 0 800 200"
       className="w-full shrink-0 bg-white stroke-black transition dark:bg-black dark:stroke-white"
     >
-      <circle cx="50%" cy="50%" r={valueA * 0.5} fill="black" />
-      <circle cx="50%" cy="50%" r={valueD * 0.5} fill="white" />
+      <defs>
+        <pattern id="ring" width="5%" height="20%">
+          <circle cx="20" cy="20" r={normalizedValueA} fill="black" />
+        </pattern>
+      </defs>
+      <rect width="800" height="200" fill="url(#ring)" stroke="transparent" />
     </svg>
   );
 }
